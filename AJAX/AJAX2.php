fc06383f1f43e4affc341a2abd00b80d5544cb2b
xenlord <alopezca16dw@ikzubirimanteo.com>
2017-11-14 11:13:24 +0100

    Ejercicios ajax

---
+++ AJAX/AJAX2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xenlord
+ * Date: 14/11/17
+ * Time: 10:37
+ */
+
+$fichero = 'AJAX2.xml';
+
+if(file_exists($fichero)){
+    $output = file_get_contents($fichero);
+    header('Content-Type: applicattion/XML');
+    print ($output);
+} else{
+    print("No existe el fichero");
+}
+
+
+
+?>
